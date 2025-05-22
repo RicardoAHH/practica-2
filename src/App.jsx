b812c2cb2e4da5cd90af1c2e131be3b26a0d301d
Ricardo Hernandez <richhquim@gmail.com>
2025-05-22 15:41:34 -0600

    api

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
 import { Input } from './components/input'
 import { Button } from './components/button'
+import Pexels from './components/pexels'
+import Cards from './components/cards'
+import PexelsPhotos from './components/PexelsPhotos'
+// import { createClient } from 'pexels'
+
+// const client = createClient('JNmdMAkd4EopN8OnxSkEZ9es3QFQuKsdn9XAWvRRrO3CwVLOIG17pb4c');
 
 export default function App() {
+
+
   return (
     <>
-
-      <Input className='border-3' />
-      <div className='flex w-full h-25 items-center justify-center'><Button contenido="boton1" /><Button contenido="boton2" /><Button contenido="boton3" /><Button contenido="boton4" /></div>
+      <div className='w-full h-full bg-gray-900'>
+        <Pexels />
+        <Input />
+        <div className='flex w-full h-25 items-center justify-center'>
+          <Button contenido="Mountain" color="blue" />
+          <Button contenido="Beaches" color="green" />
+          <Button contenido="Birds" color="red" />
+          <Button contenido="Foods" color="gray" />
+        </div>
+        <div>
+          {/* <Cards /> */}
+          <PexelsPhotos />
+        </div>
+      </div>
     </>
   )
 }
