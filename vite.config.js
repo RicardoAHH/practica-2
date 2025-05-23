fc932f234092f8ae6289eba99935dcfc7ffc4b63
Ricardo Hernandez <richhquim@gmail.com>
2025-05-23 15:11:40 -0600

    deployfixed

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-
+  base: '/practica-2',
+  optimizeDeps: { include: ['pexels'] },
+  build: {
+    commonjsOptions: { transformMixedEsModules: true }
+  }
 })
